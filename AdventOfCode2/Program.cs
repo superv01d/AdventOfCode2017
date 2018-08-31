554fbcd886fec1e2da1c345f6c398808969e80da
superv01d <42724131+superv01d@users.noreply.github.com>
2018-09-01 00:53:29 +0300

    Day 2

---
+++ AdventOfCode2/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Priority_Queue;
+
+namespace AdventOfCode2
+{
+    class Program
+    {
+        static List<string> readInput(string filename)
+        {
+            List<string> result = new List<string>();
+            string s;
+            using (var sr = File.OpenText(filename))
+            {
+                while ((s = sr.ReadLine()) != null)
+                {
+                    result.Add(s);
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            List<string> inputList = readInput("input.txt");
+            var sum = 0;
+            foreach (var item in inputList)
+            {
+                var arr = item.Split(new char[] { '\t' }).Select(c => int.Parse(c)).OrderBy(i => i).ToArray();
+                var isFound = false;
+                for (int i = 0; i < arr.Count() - 1; i++)
+                {
+                    for (int j = i + 1; j < arr.Count(); j++)
+                    {
+                        if (arr[j] % arr[i] == 0)
+                        {
+                            sum += arr[j] / arr[i];
+                            isFound = true;
+                            break;
+                        }
+                    }
+                    if (isFound)
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+
+        #region Helpers
+
+        private static string getMD5(string s)
+        {
+            var md5 = MD5.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(s);
+            var hash = md5.ComputeHash(inputBytes);
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString().ToLower();
+        }
+
+        public class Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public override string ToString()
+            {
+                return string.Format("{0}:{1}", X, Y);
+            }
+
+            public override bool Equals(object obj)
+            {
+                var pointObj = obj as Point;
+                return (X.Equals(pointObj.X) && Y.Equals(pointObj.Y));
+            }
+
+            public override int GetHashCode()
+            {
+                return ToString().GetHashCode();
+            }
+        }
+
+        #endregion
+
+    }
+}
